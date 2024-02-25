6d7ec952942c85dc9d630dc0788ae2a1c5b58abe
Anand Vishwakarma <anand.vish201@gmail.com>
2024-02-25 13:17:29 +0530

    json not working, hard coded json for now

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,57 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import DoctorCard from "./DoctorCard";
 
-export default function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+const details = [
+  {
+    name: "Dr. Name",
+    img: "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+    detail: "lorem ipsum",
+  },
+  {
+    name: "Dr. Name",
+    img: "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+    detail: "lorem ipsum",
+  },
+  {
+    name: "Dr. Name",
+    img: "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+    detail: "lorem ipsum",
+  },
+  {
+    name: "Dr. Name",
+    img: "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+    detail: "lorem ipsum",
+  },
+  {
+    name: "Dr. Name",
+    img: "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+    detail: "lorem ipsum",
+  },
+  {
+    name: "Dr. Name",
+    img: "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+    detail: "lorem ipsum",
+  },
+];
 
-  useEffect(() => {
-    setLoading(true);
-    fetch("http://localhost:9000/details")
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => console.error(err))
-      .finally(setLoading(false));
-  }, []);
+export default function App() {
+  const [data, setData] = useState(details);
+  // const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch("http://localhost:9000/details")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data))
+  //     .catch((err) => console.error(err))
+  //     .finally(setLoading(false));
+  // }, []);
 
   return (
     <div>
       <nav className="secondary-tab">Select Doctor</nav>
-      {loading ? <p>Loading...</p> : <DoctorCard data={data} />}
+      <DoctorCard data={data} />
+      {/* {loading ? <p>Loading...</p> : <DoctorCard data={data} />} */}
     </div>
   );
 }
